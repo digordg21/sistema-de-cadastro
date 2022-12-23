22b743dad4d20e8a7a05bc582b6bdb0a4fe3a381
Abed De Andrade <rabed@indracompany.com>
2022-12-23 18:30:59 -0300

    adicionando timestamp

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
-console.log("Rodando o sistema de gerenciamento de jogos")
-console.log("Aplicação está se conectando ao banco de dados")
-console.time
+console.log("Rodando o sistema de gerenciamento de jogos");
+console.log("Aplicação está se conectando ao banco de dados");
+// new Date object
+const dateObject = new Date();
+
+// current date
+// adjust 0 before single digit date
+const date = (`0 ${dateObject.getDate()}`).slice(-2);
+ 
+// current month
+const month = (`0 ${dateObject.getMonth() + 1}`).slice(-2);
+ 
+// current year
+const year = dateObject.getFullYear();
+ 
+// current hours
+const hours = dateObject.getHours();
+ 
+// current minutes
+const minutes = dateObject.getMinutes();
+ 
+// current seconds
+const seconds = dateObject.getSeconds();
+ 
+// prints date in YYYY-MM-DD format
+console.log(`${year}-${month}-${date}`);
+ 
+// prints date & time in YYYY-MM-DD HH:MM:SS format
+console.log(`${year}-${month}-${date} ${hours}:${minutes}:${seconds}`);
+ 
+// prints time in HH:MM format
+console.log(`${hours}:${minutes}`);
